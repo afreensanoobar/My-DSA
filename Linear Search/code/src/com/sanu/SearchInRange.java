abe252b9e5933b89eb47189d03aba55a010fd2fc
afreensanoobar <afreensanoobar0026@gmail.com>
2026-02-24 00:21:43 +0530

    O

--- Linear Search/code/src/com/sanu/SearchInRange.java
+++ Linear Search/code/src/com/sanu/SearchInRange.java
@@ -3,12 +3,4 @@ package com.sanu;
 public class SearchInRange {
     public static void main(String[] args) {
         int [] arr ={1,56,67,89,90,45,34,};
-        int target =90;
-        System.out.println(linearSearch(arr, target, 1,4));
-    }
- static int linearSearch (int []arr, int target , int start, int end){
-        if(arr.isEmpty){
-            retyurn
-        }
- }
-}
+        int target
