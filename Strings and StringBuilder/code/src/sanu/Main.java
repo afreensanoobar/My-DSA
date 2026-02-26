1b09c17daf7f65ed850a073f395c7784094f0b1b
afreensanoobar <afreensanoobar0026@gmail.com>
2026-02-26 22:42:02 +0530

    corrected

--- Strings and StringBuilder/code/src/sanu/Main.java
+++ Strings and StringBuilder/code/src/sanu/Main.java
@@ -1,3 +1,9 @@
 package sanu;
 
-pub
+public class Main{
+    public static void main(String[] args) {
+        int [] arr ={2,3,4,5};
+        String name = "Sanoobar Afreen";
+        System.out.println(name);
+    }
+}
