9f8c9568deb352f413f8f193231dca152e0eca86
afreensanoobar <afreensanoobar0026@gmail.com>
2026-02-27 23:45:42 +0530

    P

--- Strings and StringBuilder/code/src/sanu/Main.java
+++ Strings and StringBuilder/code/src/sanu/Main.java
@@ -1,3 +1,9 @@
 package sanu;
 
-publ
+public class Main{
+    public static void main(String[] args) {
+        int [] arr ={2,3,4,5};
+        String name = "Sanoobar Afreen";
+        System.out.println(name);
+    }
+}
