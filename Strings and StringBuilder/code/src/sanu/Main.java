b4bac58a09c2daf8e094048e85476c7abced2a5b
afreensanoobar <afreensanoobar0026@gmail.com>
2026-03-15 23:55:57 +0530

    change

--- Strings and StringBuilder/code/src/sanu/Main.java
+++ Strings and StringBuilder/code/src/sanu/Main.java
@@ -2,8 +2,5 @@ package sanu;
 
 public class Main{
     public static void main(String[] args) {
-        int [] arr ={2,3,4,5};
-        String name = "Sanoobar Afreen";
-        System.out.println(name);
-    }
+        int [] a
 }
