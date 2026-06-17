8f64f93ca525746d6a736d487cb34cf6dc3a53ac
Sanoobar Afreen <afreensanoobar0026@gmail.com>
2026-06-17 19:22:44 +0530

    Array and ArrayList

--- Array/Arrays/src/NegativeNumbers.java
+++ Array/Arrays/src/NegativeNumbers.java
@@ -1,2 +1,20 @@
+
+import java.util.Scanner;
+
 public class NegativeNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter arr size");
+        int n = sc.nextInt();
+        int [] arr = new int [n];
+        System.out.println("enter array elements");
+        for (int i=0; i<n  ; i++) {
+            arr[i] = sc.nextInt();
+        }
+        //printing
+        for (int i=0; i<n; i++) {
+            if (arr[i] < 0)
+                System.out.print(arr[i] + " ");
+        }
+    }
 }
