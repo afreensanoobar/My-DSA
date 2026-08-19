be8e9fa2a1153ff40a3cdbff13151f2bf30c131d
Sanoobar Afreen <afreensanoobar0026@gmail.com>
2026-08-19 23:58:26 +0530

    Stacks

---
+++ Doubly Linked List/code/src/com/doublyList/DoublyLinkedList.java
@@ -0,0 +1,4 @@
+package com.doublyList;
+
+public class DoublyLinkedList {
+}
---
+++ Stacks/code/src/com/stacks/PushAtBottomRecur.java
@@ -0,0 +1,34 @@
+package com.stacks;
+import java.util.Stack;
+
+public class PushAtBottomRecur {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        int ele = 50;
+        System.out.println(st);
+        pushAtBottom(st,ele);
+        System.out.println(st);
+        reverse(st);
+        System.out.println(st);
+    }
+    private static void reverse(Stack<Integer> st){
+        if(st.size()<=1) return ;
+        int top = st.pop();
+        reverse(st);
+        pushAtBottom(st,top);
+    }
+      public static void  pushAtBottom(Stack<Integer> st ,int ele){
+        if(st.isEmpty()){
+        st.push(ele);
+        return;
+
+          }
+         int top = st.pop();
+           pushAtBottom(st,ele);
+          st.push(top);
+        }
+        }
